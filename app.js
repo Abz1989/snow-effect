e9c34e5e21434f7260ab4724ff4e1f6d09ece741
Abz1989 <saqibquadir@gmail.com>
2022-07-29 21:21:47 +0100

    all the files are alright

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-createSnowFlake();
+setInterval(createSnowFlake, 100);
 
 function createSnowFlake() {
     const snow_flake = document.createElement('i');
-    snow_flake.classList.add('.fa-solid');
-    snow_flake.classList.add('.fa-snowflake');
+    snow_flake.classList.add('fa-solid');
+    snow_flake.classList.add('fa-snowflake');
     snow_flake.style.left = Math.random() * window.innerWidth + 'px';
 
 
-    body.append.Child(snow_flake);
+    document.body.appendChild(snow_flake);
+
+    setTimeout(() => {
+        snow_flake.remove();
+    }, 5000)
 }
