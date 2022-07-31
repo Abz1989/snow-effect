73f481c6a4a16827681791ef5bfa3eed5a6c057a
Abz1989 <saqibquadir@gmail.com>
2022-07-31 22:18:06 +0100

    All javascript are fixedcan play with it ana make the project expand in the future

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ function createSnowFlake() {
     snow_flake.classList.add('fa-solid');
     snow_flake.classList.add('fa-snowflake');
     snow_flake.style.left = Math.random() * window.innerWidth + 'px';
+    snow_flake.style.animationDuration = Math.random() * 2 + 3 + 's';
+    snow_flake.style.opacity = Math.random();
+    snow_flake.fontSize = Math.random() * 10 + 10 + 'px';
 
 
     document.body.appendChild(snow_flake);
